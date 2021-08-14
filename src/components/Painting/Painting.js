cffc0d28b85349fba9bff8496fd233467e51358d
ferica <67454164+ferica@users.noreply.github.com>
2021-08-14 13:18:00 -0700

    painting page

--- src/components/Painting/Painting.js
+++ src/components/Painting/Painting.js
@@ -1,5 +1,6 @@
 import './Painting.css';
-import Ferica from '../About/Ferica.jpg';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
 import painting1 from './painting1.jpeg';
 import painting2 from './painting2.jpeg';
 import painting3 from './painting3.jpeg';
@@ -7,7 +8,6 @@ import painting4 from './painting4.jpeg';
 import painting5 from './painting5.jpeg';
 import painting6 from './painting6.jpeg';
 import painting7 from './painting7.jpg';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 
 
 const Painting = () => {
@@ -29,102 +29,6 @@ const Painting = () => {
                 <img src={painting6} alt='painting6'/>
                 <img src={painting7} alt='painting7'/>
               </div>
-
-  {/* <div class='row'>    
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"Loneliness of autumn"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"Happiness"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"Last Dream"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"New Big Ben"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class='row'>    
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"Autumn Foggy Day"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"Paris, Eifel Tower"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"He proposal"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-    <div class='thumbnail'>
-      <div class='photoContainer'>
-        <a href='#'>
-        <img src={Ferica} alt='Ferica-img'/>
-          <div class='photoInfo'>
-            <h3>"The Bridge of Amsterdam"</h3>
-            <span class='paintingDate'>Leonid Afremov</span>
-          </div>          
-        </a>
-      </div>
-    </div>
-  
-  
-</div>        */}
-
             </div>
         </div>
      );
