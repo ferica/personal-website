02711633492b961bde4f45842e550e17861902d0
ferica <67454164+ferica@users.noreply.github.com>
2021-08-16 11:32:38 -0700

    home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,14 +9,23 @@ const Home = () => {
         <div className="home-page">
             <div className="home-container">
 
-                <div className="wrapper">
+                {/* <div className="wrapper">
                     <div className="typing-container">
-                        <p id="typing-text">Hi, I'm Ferica.</p> 
+                        <p className="typing-text">Hi, I'm Ferica.</p> 
                     </div>
+                </div> */}
 
+                <div className="content">
+                    <p className="typed">Hi, I'm Ferica.</p>
+                    <p className="typed">// I code ideas.</p>
+
+                    <div className="icons-container">
+                        <a href="https://www.linkedin.com/in/ferica-ting-6141511b0/"><LinkedInIcon className="icon" id="linkedin" fontSize="large"/></a>
+                        <a href="https://github.com/ferica"><GitHubIcon className="icon" id="github" fontSize="large" /></a>
+                    </div>
                 </div>
-                
-            </div>      
+                 
+            </div>   
         </div>
       );
 }
