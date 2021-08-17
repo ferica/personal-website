f108cfeb47163f6364e25fb68975d72b8ab21894
ferica <67454164+ferica@users.noreply.github.com>
2021-08-16 18:44:49 -0700

    updated project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,12 @@ import ProjectItem from './ProjectItem';
 import Website_Image from './Website_Image.PNG';
 import Ecommerce_Image from './Ecommerce_Image.PNG';
 import placeholder from './placeholder.PNG';
-
 import GitHubIcon from '@material-ui/icons/GitHub';
 import CallMadeIcon from '@material-ui/icons/CallMade';
 
 
 const Projects = () => {
+
     return ( 
         <div className="projects-page">
             <div className="projects-container">
@@ -19,7 +19,7 @@ const Projects = () => {
                     <ProjectItem image={Website_Image} 
                     projectName="Personal Website" 
                     description="Designed and implemented my own personal website to showcase my resume."
-                    tools="Javascript, HTML/CSS, React.js"
+                    tools="Javascript, HTML/CSS, React.js, Figma"
                     github={true} view={true} 
                     githubLink="https://github.com/ferica/personal-website"
                     viewLink="/" />
@@ -28,14 +28,14 @@ const Projects = () => {
                     projectName="Ecommerce" 
                     description="Implemented an ecommerce website that mimics some of the functionalities
                     of sites like eBay. User information is logged and stored in a SQLite database."
-                    tools="Python (Django), HTML/CSS, SQLite" 
+                    tools="Python (Django), HTML/CSS" 
                     github={true}
                     githubLink="https://github.com/ferica/ecommerce" />
 
 
 
-                    <ProjectItem image={placeholder} projectName="Placeholder" description="testing" />
-         
+                    {/* <ProjectItem image={placeholder} projectName="Placeholder" description="testing" /> */}
+           
                 </div>
             </div>
         </div>
